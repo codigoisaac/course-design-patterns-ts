5863bc3477849b899caa26da7e2227b2329d1b39
codigoisaac <isaacmuniz@protonmail.com>
2024-08-12 18:23:33 -0300

    create client class

---
+++ single_responsibility/src/Client.ts
@@ -0,0 +1,17 @@
+export default class Client {
+  create(): void {
+    console.log("Client created");
+  }
+
+  read(): void {
+    console.log("Client read");
+  }
+
+  update(): void {
+    console.log("Client updated");
+  }
+
+  delete(): void {
+    console.log("Client deleted");
+  }
+}
